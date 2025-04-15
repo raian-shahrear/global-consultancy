f44efc96e764051c9ccf1782caa6721c8580c164
raianshahrear <raian@nextgenitltd.com>
2025-04-16 03:28:13 +0600

    initialization

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <div className='flex justify-center items-center h-screen'>
+      <h1 className='text-center font-bold text-4xl'>Hello from new app</h1>
+    </div>
+  )
+}
+
+export default App
