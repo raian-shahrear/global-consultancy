c74bb24ba49322d66232cbd8e05d373b56bbc6b1
Raian Shahrear <raianshahrearbd@gmail.com>
2025-05-06 19:03:39 +0600

    Shadcn/ui added

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
-import './App.css'
+import { Button } from "./components/ui/button";
 
 function App() {
-
   return (
-    <div className='flex justify-center items-center h-screen'>
-      <h1 className='text-center font-bold text-4xl'>Hello from new app</h1>
+    <div className="flex justify-center items-center h-screen">
+      <div className="flex flex-col justify-center items-center gap-4">
+        <h1 className="text-center font-bold text-4xl">Hello from new app</h1>
+        <Button>Click</Button>
+      </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
